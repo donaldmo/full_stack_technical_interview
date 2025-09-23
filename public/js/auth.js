8745e7773e7bb4e4e2a0fb9adc4a457bd76b4045
Donald Motswiri <domotswiri@gmail.com>
2025-09-23 18:28:30 +0200

    feat: implement registration functionality with form validation and error handling

--- public/js/auth.js
+++ public/js/auth.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
   const loginForm = document.getElementById('login-form');
+  const registerForm = document.getElementById('register-form');
 
   if (loginForm) {
     loginForm.addEventListener('submit', async (e) => {
@@ -34,6 +35,53 @@ document.addEventListener('DOMContentLoaded', () => {
           }
         } catch (error) {
           console.error('Error logging in:', error);
+          errorMessage.textContent = 'An error occurred.';
+        }
+      }
+    })
+  }
+
+  if (registerForm) {
+    registerForm.addEventListener('submit', async (e) => {
+      e.preventDefault();
+
+      const email = registerForm.email.value;
+      const password = registerForm.password.value;
+      const confirmPassword = registerForm['confirm-password'].value;
+      const name = registerForm.name.value;
+    
+      const errorMessage = document.getElementById('error-message');
+
+      if (password !== confirmPassword) {
+        errorMessage.textContent = 'Passwords do not match';
+        return;
+      }
+
+      if (name && email && password) {
+        try {
+          const response = await fetch('/api/auth/register', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ name, email, password })
+          });
+
+          console.log('response: ', response)
+
+          if (response.ok) {
+            const data = await response.json();
+            console.log('data: ', data)
+
+            if (data.accessToken) {
+              localStorage.setItem('auth_token', data.accessToken);
+              window.location.href = '/';
+            }
+          } else {
+            errorMessage.textContent = error.message || 'Registration failed';
+          }
+        } catch (error) {
+          console.error('Error registering:', error);
           errorMessage.textContent = 'An error occurred.';
         }
       }
